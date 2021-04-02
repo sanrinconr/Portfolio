b5b62e4d41fbf0b85d834899a1e6939bb1780a74
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-01 22:41:25 -0500

    Added horizontal line in skill

--- client/src/pages/Home/components/Section/Skill/SkillView.jsx
+++ client/src/pages/Home/components/Section/Skill/SkillView.jsx
@@ -2,6 +2,7 @@ import styles from "./SkillView.module.css"
 export function SkillView({title, image}){
     return <div className={styles.container}>
         <img src={image} alt=""/>
+        <hr/>
         <h2>{title}</h2>
     </div>
 }

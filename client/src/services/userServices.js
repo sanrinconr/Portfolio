28369f7753586b304f985c51f4e26c0e8c014d54
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-03 02:14:24 -0500

    Se opta por utilizar post
    Se estaba teniendo problemas con la codificacion de caracteres

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -2,9 +2,13 @@ import axios from "axios"
 export function sendMessage(message, where_contact){
     const TOKEN = process.env.REACT_APP_BOT_TELEGRAM_TOKEN
     const CHAT_ID = process.env.REACT_APP_CHAT_ID_SANTIAGO
-    const detailMessage = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}%0a %0a${where_contact}`
+    const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+    // const detailMessage = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&parse_mode=HTML&text=${message}%0a %0a${where_contact}`
 
-    return axios.get(detailMessage)
+    return axios.post(URL,{
+        chat_id:CHAT_ID,
+        text:`Message: ${message}\n \nContact info:\n ${where_contact}`
+    })
     .then((res)=>{
         return res
     })

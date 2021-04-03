a930958579eac36d35da53034d3ec4e77949fd3e
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-02 20:25:32 -0500

    Added icons

--- client/src/redux/reducers/skillReducer.js
+++ client/src/redux/reducers/skillReducer.js
@@ -6,6 +6,15 @@ import sequelizeLogo from "../../assets/images/sequelizeLogo.svg"
 import postgresqlLogo from "../../assets/images/postgresqlLogo.svg"
 import gitLogo from "../../assets/images/gitLogo.svg"
 
+
+import arrayIcon from "../../assets/images/arrayIcon.svg"
+import treeIcon from "../../assets/images/treeIcon.svg"
+import hasgTableIcon from "../../assets/images/hashTableIcon.svg"
+import linkedListIcon from "../../assets/images/linkedListIcon.svg"
+import complexityAnalysisIcon from "../../assets/images/complexityAnalysisIcon.svg"
+import sortIcon from "../../assets/images/sortIcon.svg"
+import stackQueueIconIcon from "../../assets/images/stackQueueIcon.svg"
+
 const initialState = {
     tools:[
         {name:"React",img:reactLogo},
@@ -17,13 +26,13 @@ const initialState = {
         {name:"Git",img:gitLogo}
         ],
     theoretical:[
-        {name:"A",img:reactLogo},
-        {name:"A",img:reduxLogo},
-        {name:"Node",img:nodeLogo},
-        {name:"Express",img:expressLogo},
-        {name:"Sequelize",img:sequelizeLogo},
-        {name:"PostgreSQL",img:postgresqlLogo},
-        {name:"Git",img:gitLogo}
+        {name:"Matrix, array",img:arrayIcon},
+        {name:"Graphs, trees",img:treeIcon},
+        {name:"Hash tables",img:hasgTableIcon},
+        {name:"Linked lists",img:linkedListIcon},
+        {name:"Complexity analysis",img:complexityAnalysisIcon},
+        {name:"Search and sort",img:sortIcon},
+        {name:"Stacks and queues",img:stackQueueIconIcon}
         ]
 }
 

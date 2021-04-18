ce286b98060beb3d82d80c405b1ee25b5b194961
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-18 03:34:55 -0500

    Added data to diferent theorical skills

--- client/src/redux/reducers/skillReducer.js
+++ client/src/redux/reducers/skillReducer.js
@@ -17,6 +17,11 @@ import stackQueueIconIcon from "../../assets/images/stackQueueIcon.svg"
 
 
 import arrayAnimation from "../../assets/theoretical/arrays/arrayAnimation.gif"
+import treeAnimation from "../../assets/theoretical/tree/treeAnimation.gif"
+import hashAnimation from "../../assets/theoretical/hashTable/3sumAnimation.gif"
+import linkedListAnimation from "../../assets/theoretical/linkedList/linkedListAnimation.gif"
+import searchAnimation from "../../assets/theoretical/search/searchAnimation.gif"
+
 
 const initialState = {
 	tools: [
@@ -61,15 +66,30 @@ const initialState = {
 		},
 		{
 			name: "Graphs, trees",
-			img: treeIcon
+			img: treeIcon,
+			description: `Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.`,
+			idea:`The maner to afront this exercise is with the idea of "Insert the two arrays in a tree (log(n)), after, get a array result of inorden and get the medium of this array, return this value".`,
+			gitLink:`https://github.com/sanrinconr/LeetCode-exercises/tree/main/Binary%20search/Median%20of%20Two%20Sorted%20Arrays`,
+			animation:treeAnimation,
+			leetCodeLink: `https://leetcode.com/problems/median-of-two-sorted-arrays/`
 		},
 		{
 			name: "Hash tables",
-			img: hasgTableIcon
+			img: hasgTableIcon,
+			description: `Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.`,
+			idea:`The idea is based in the third number is the result of the sum of two other numbers, with this in mind, the process was: sum the actual element of first "for" and second "for", then if hashtable has the inverse (with -), the triplet has been discovered!`,
+			gitLink:`https://github.com/sanrinconr/LeetCode-exercises/tree/main/HashTable/3Sum`,
+			animation:hashAnimation,
+			leetCodeLink:`https://leetcode.com/problems/3sum/`,
 		},
 		{
 			name: "Linked lists",
-			img: linkedListIcon
+			img: linkedListIcon,
+			description: `You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.`,
+			idea:`The maner to afront this exercise is with the idea of "sum the two actual nodes, push the result in a array, and go to the next of every list until finish. Finally use this result array to create new linked list".`,
+			gitLink:`https://github.com/sanrinconr/LeetCode-exercises/tree/main/LinkedList/Add%20two%20numbers`,
+			animation:linkedListAnimation,
+			leetCodeLink:`https://leetcode.com/problems/add-two-numbers/`
 		},
 		{
 			name: "Complexity analysis",
@@ -77,7 +97,12 @@ const initialState = {
 		},
 		{
 			name: "Search and sort",
-			img: sortIcon
+			img: sortIcon,
+			description: `Given a string s, find the length of the longest substring without repeating characters.`,
+			idea: `The maner to afront this exercise is with the idea of "sum the two actual nodes, push the result in a array, and go to the next of every list until finish. Finally use this result array to create new linked list".`,
+			gitLink:`https://github.com/sanrinconr/LeetCode-exercises/tree/main/Strings/Longest%20Substring%20Without%20Repeating%20Characters`,
+			animation:searchAnimation,
+			leetCodeLink: `https://leetcode.com/problems/longest-substring-without-repeating-characters/`
 		},
 		{
 			name: "Stacks and queues",

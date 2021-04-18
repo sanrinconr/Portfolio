b612cae7208cb0e1ce9a25290041f798a1c2a2f1
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-18 02:00:41 -0500

    Hiperlink color in mobile,

--- client/src/pages/SkillTheoretical/SkillTheoreticalView.jsx
+++ client/src/pages/SkillTheoretical/SkillTheoreticalView.jsx
@@ -4,23 +4,28 @@ import githubLogo from "../../assets/theoretical/githubLogo.svg"
 import leetCodeLogo from "../../assets/theoretical/leetCodeLogo.svg"
 import { useEffect } from "react";
 
-export function SkillTheoreticalView({name, description,img, idea, animation,gitLink}){
-    // useEffect(() => {
-    //     window.scrollTo(0, 0);
-    //   }, []);
+export function SkillTheoreticalView({name, description,img, idea, animation,gitLink, leetCodeLink}){
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
     return <div>
         <Section theme="black" title={name}>
             <div className={styles.img}>
                 <img src={img} alt=""/>
             </div>
         </Section>
-        <Section theme="white" title={`How can i show the skill?. LeetCode problem!`}>
+        <Section theme="white" title={`How can i show the skill?. A Leet code problem!`}>
             <div className={styles.description}>
                 <img src={leetCodeLogo} alt=""/>
                 <div>
                     <p>{description}</p>
                 </div>
             </div>
+            <div className={styles.goToLeetCode}>
+                    <a href={leetCodeLink} target="_blank" rel="noreferrer">
+                    (click here if you wanna see the problem in leetcode.com)
+                    </a>
+                </div>
         </Section>
         <Section theme="black" title="How i solved?..">
             <div className={styles.descriptionSolution}>
@@ -33,7 +38,7 @@ export function SkillTheoreticalView({name, description,img, idea, animation,git
         <Section theme="white">
             <a href={gitLink} target="_blank" rel="noreferrer"> 
                 <div className={styles.linkToSolution}>
-                    <p>Get full solution in github!</p>
+                    <p>Click here to get full solution in github!</p>
                     <img className={styles.img} src={githubLogo} alt=""/>
                 </div>
             </a>
